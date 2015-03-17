25b1ed6b336dd0bfa5cd1a6d9e03a2ae0b64af5a
Nicole Cano <arbolkiri@gmail.com>
2015-03-16 23:05:56 -0500

    viewsnotworking

--- js/TxMx.js
+++ js/TxMx.js
@@ -4,25 +4,69 @@
 Backbone.TxMxRouter = Backbone.Router.extend({
     initialize: function(){
         console.log('routerinitialized');
-        this.homeview = new Backbone.HomeView();
+        this.model = new Backbone.ClassModel();
+        this.homeview = new Backbone.HomeView({
+            model: this.model
+        });
+        this.collection = new Backbone.ClassList();
+        this.classview = new Backbone.ClassView();
         Backbone.history.start();
     },
 
     routes: {
 
-        "viewMark": "homepageMark",
-        "viewClasses": "homepageClasses",
+        "viewMark": "Mark",
+        "viewClasses": "classes",
+        "collection": "collection",
         "*default": "homepage"
     },
     homepage: function(){
         this.homeview.render();
+    },
+    Mark: function(){
+        this.markview.render();
+    },
+    classes: function(){
+        this.classview.render();
+    },
+    collection: function(){
+        var self = this;
+        this.collection.fetch();
     }
+
 })
 
 Backbone.HomeView = Backbone.TemplateView.extend({
     el: ".container",
     view: "homepage"
 })
+
+Backbone.ClassModel = Backbone.Model.extend({
+    defaults: {
+        "classes": "false",
+        "title": "not title given",
+        "chvritbtn": "false"
+    }
+})
+
+Backbone.ClassList = Backbone.Collection.extend({
+    model: Backbone.Classmodel
+})
+
+Backbone.ClassView = Backbone.TemplateView.extend({
+    el: ".container",
+    view: "stntClassView"
+    // events: {
+    //     "click #": "trainingViewPage"
+    // }
+    // trainingViewPage: function(event){
+    //     event.preventDefault();
+    //     var x = {
+    //         chvritbtn: this.el.querySelector("a[name='rightbtn']").value
+    //     }
+    // }
+})
+
 
 
 
